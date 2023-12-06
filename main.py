b38d3cfe495cc996982769db2eb87e816217e8c1
Alexandre Corcos <acorcos@gmail.com>
2023-12-06 12:17:20 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+from datetime import datetime
+
+USERNAME = "alexandrecorcos"
+TOKEN = "adsfjaslkdf765dsf7a"
+GRAPH_ID = "graph1"
+
+# today = datetime.now()
+today_now = datetime(year=2023, month=12, day=5)
+TODAY = today_now.strftime("%Y%m%d")
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_running_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+graph_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{TODAY}"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Running Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+graph_insert = {
+    "date": TODAY,
+    "quantity": input("How many kilometers did you run today? ")
+}
+
+graph_update = {
+    "quantity": "5"
+}
+
+# POST
+# response = requests.post(url=graph_running_endpoint, json=graph_insert, headers=headers)
+# print(response.text)
+
+# UPDATE
+# response = requests.put(url=graph_update_endpoint, json=graph_update, headers=headers)
+# print(response.text)
+
+# DELETE
+# response = requests.delete(url=graph_update_endpoint, json=graph_update, headers=headers)
+# print(response.text)
