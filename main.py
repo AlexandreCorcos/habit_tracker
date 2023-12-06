ccd0460a3ef0643882150d0e3d203b0c15da9002
Alexandre Corcos <acorcos@gmail.com>
2023-12-06 12:55:48 +0000

    insert menu

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ USERNAME = "alexandrecorcos"
 TOKEN = "adsfjaslkdf765dsf7a"
 GRAPH_ID = "graph1"
 
-# today = datetime.now()
-today_now = datetime(year=2023, month=12, day=5)
-TODAY = today_now.strftime("%Y%m%d")
+now = datetime.now()
+TODAY = now.strftime("%Y%m%d")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -18,7 +17,7 @@ user_params = {
     "notMinor": "yes"
 }
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_running_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 graph_update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{TODAY}"
 
@@ -34,14 +33,36 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-graph_insert = {
-    "date": TODAY,
-    "quantity": input("How many kilometers did you run today? ")
-}
+
 
 graph_update = {
     "quantity": "5"
 }
+
+choice = input("What you want to do? (I)nsert/(U)pdate/(D)elete: ").lower().strip()
+
+if choice == "insert" or choice == "i":
+
+    graph_insert = {
+        "date": TODAY,
+        "quantity": input("How many kilometers did you run today? ")
+    }
+
+    response = requests.post(url=graph_running_endpoint, json=graph_insert, headers=headers)
+    print(response.text)
+elif choice == "update" or choice == "u":
+    graph_update = {
+        "quantity": input("How many kilometers did you run today? ")
+    }
+
+    response = requests.put(url=graph_update_endpoint, json=graph_update, headers=headers)
+    print(response.text)
+elif choice == "delete" or choice == "d":
+    response = requests.delete(url=graph_update_endpoint, json=graph_update, headers=headers)
+    print(response.text)
+else:
+    print("Choose a valid option")
+
 
 # POST
 # response = requests.post(url=graph_running_endpoint, json=graph_insert, headers=headers)
